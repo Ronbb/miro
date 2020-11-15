81ac71a57467d3e0185d8c327a88609a738e821f
ronbb <ronbiaobiao@gmail.com>
2020-11-15 22:48:12 +0800

    Initial Commit

---
+++ miro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  miro
+//
+//  Created by 不会说话的猫 on 2020/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ miro/miroApp.swift
@@ -0,0 +1,17 @@
+//
+//  miroApp.swift
+//  miro
+//
+//  Created by 不会说话的猫 on 2020/11/15.
+//
+
+import SwiftUI
+
+@main
+struct miroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
